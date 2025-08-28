a6c799a52775be6f4c5384499ce7a9c1d72268ab
flaviozno <flavio.vezono@ufu.br>
2025-08-28 13:17:34 -0300

    next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "instagram.*", 
+      },
+    ],
+  },
+};
 
 export default nextConfig;
